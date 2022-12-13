c99acee7df36fd37771a44b8c18e91b5c0be644d
Daijoubu22 <alextrio.2224@gmail.com>
2022-12-13 23:18:01 +0300

    Add images proxy

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+import corsProxy from 'cors-anywhere';
+import express from 'express';
+import axios from 'axios';
 
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-	originWhitelist: [], // Allow all origins
+const host = process.env.HOST || '0.0.0.0';
+const corsPort = process.env.PORT || 8080;
+const imagesProxyPort = process.env.PORT || 8081;
+
+const imagesProxy = express();
+
+function getBase64(url) {
+	return axios
+		.get(url, {
+			responseType: 'arraybuffer'
+		})
+		.then(response => Buffer.from(response.data, 'binary').toString('base64'))
+}
+
+imagesProxy.get('/', (req, res) => {
+	getBase64(req.query.url).then((data) => {
+		const img = Buffer.from(data, 'base64');
+		res.writeHead(200, {
+			'Content-Type': 'image/png',
+			'Content-Length': img.length
+		});
+		res.end(img);
+	})
+});
+
+imagesProxy.listen(imagesProxyPort, host, () => {
+	console.log(`Running Images Proxy on ${host}:${imagesProxyPort}`)
+});
+
+corsProxy.createServer({
+	originWhitelist: [],
 	requireHeader: ['origin', 'x-requested-with'],
 	removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-	console.log('Running CORS Anywhere on ' + host + ':' + port);
+}).listen(corsPort, host, function() {
+	console.log('Running CORS Anywhere on ' + host + ':' + corsPort);
 });
